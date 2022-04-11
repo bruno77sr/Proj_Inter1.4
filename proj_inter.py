2bdda997e71872065b2966d53b852e6b1b15f417
Logon Anália Franco <bruno77sr@gmail.com>
2022-04-11 19:16:53 -0300

    att

--- proj_inter.py
+++ proj_inter.py
@@ -1,24 +1,32 @@
+
 #Menu/ Entrada de dados
+print (30 * '-')
+print('\033[30;41mPROJETO INTERDISCIPLINAR 1.0 \033[30;41m') 
+print (30 * '-')
 
-print("PROJETO INTERDISCIPLINAR. \n         Dec Convert \nConversão da base decimal para as bases binário, hexadecimal e octadecimal \n")  #"Dec Convert" é para ser o nome do programa"
+print ('DEC CONVERT 1.0')
+print (30 * '-')
+print('Conversão da base decimal, para a base binário, octadecimal e hexadecimal. \n')  #"Dec Convert 1.0" Nome do programa"
 
-op =  input("Escolha uma opção: \n B - Decimal para Binário; \n H - Decimal para Hexadecimal \n O - Decimal para Octadecimal \n Digite aqui: " )
+op =  input('Escolha uma opção: \n [1] - Decimal para Binário \n [2] - Decimal para Octadecimal \n [3] - Decimal para Hexadecimal \n [4] - Sobre \n Digite aqui: ' )
+
+
 
 #Processamento
 
-if op != "B" and op != "b" and "H" and op != "h" and op != "O" and op != "o":
+if op != "1" and op != "2" and op != "3" and op != "4":
     print ("Opção invalida! ")
 else:
     dec = int(input("Digite um valor em Decimal: ")) #Entrada de dados (Número Decimal)
-    if op == "B" or op == "b":
+    if op == "1":
        resul = dec+5                            #processamento para teste
-       print("O resultado é: ", resul)
-    elif op == "H" or op == "h":
+       print("A conversão de decimal para binário é: ", resul)
+    elif op == "2":
          resul = dec+3                          #processamento para teste
-         print("O resultado é: ", resul)
-    elif op == "O" or op == "o":
+         print("A conversão de decimal para octadecimal é: ", resul)
+    elif op == "3":
          resul = dec+6                          #processamento para teste
-         print("O resultado é: ", resul)
+         print("A conversão de decimal para hexadecimal é: ", resul)
 
 
 
